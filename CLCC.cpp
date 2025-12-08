8caed3693cde6a6e468a52927fdf33e5c31868a6
vista-wang <vista_2023@icloud.com>
2025-12-08 18:23:00 +0800

    完善了基本功能（加减乘除 和 幂运算）

--- CLCC.cpp
+++ CLCC.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <string>
 #include <map>
-
-//这里引用的头文件你问DeepSeek是干嘛的，DS示例代码里加的
+#include <string>
 #include <stdexcept>
 
 using namespace std;
@@ -13,15 +11,9 @@ namespace vars{
 }
 
 namespace func{
-	int print(const string& out){
-		if(vars::vars.find(out) == vars::vars.end()){
-			cout << "error 03: undefined variables" << endl;
-			return 1;
-		}
-		cout << vars::vars[out] << endl;
-		return 0;
-	}
-	bool isnumber(const string& value, float& out){
+	//判断
+	bool isnumber(const string& value, float& out)
+	{
 		//这一段try和catch是照着DeepSeek给的示例代码写的，什么意思去问DeepSeek
 		try{
 			size_t pos = 0;
@@ -34,15 +26,99 @@ namespace func{
 			return false;
 		}
 	}
-	int set(const string& value,const string& name)
+
+	//输出
+	int print(const string& out)
 	{
-		float num = 0;
-		if(isnumber(value,num)){
-			vars::vars[name] = num;
-			cout << name << " = " << num << endl;
-		}else{
-			cout << "error 04: use non-real numbers to assign values" << endl;
+		if(vars::vars.find(out) == vars::vars.end()){
+			cout << "error 03: undefined variables" << endl;
 			return 1;
+		}
+		cout << vars::vars[out] << endl;
+		return 0;
+	}
+
+	//赋值
+	int set(const float& num,const string& name)
+	{
+		vars::vars[name] = num;
+		cout << name << " = " << num << endl;
+		return 0;
+	}
+
+	//加
+	int add(const float& num,const string& name)
+	{
+		vars::vars[name] += num;
+		cout << name << " = " << vars::vars[name] << endl;
+		return 0;
+	}
+
+	//减
+	int sub(const float& num,const string& name)
+	{
+		vars::vars[name] -= num;
+		cout << name << " = " << vars::vars[name] << endl;
+		return 0;
+	}
+
+	//乘
+	int mul(const float& num,const string& name)
+	{
+		vars::vars[name] *= num;
+		cout << name << " = " << vars::vars[name] << endl;
+		return 0;
+	}
+
+	//除
+	int div(const float& num,const string& name)
+	{
+		vars::vars[name] /= num;
+		cout << name << " = " << vars::vars[name] << endl;
+		return 0;
+	}
+
+	//方
+	int fang(const float& num,const string& name)
+	{
+		if (num < 0)
+		{
+			const float num2 = -num;
+			float i = 0;
+			while (i <= num2)
+			{
+				vars::vars[name] *= num2;
+				i += 1;
+			}
+			vars::vars[name] = 1 / vars::vars[name];
+		}else{
+			float i = 0;
+			while (i < num)
+			{
+				vars::vars[name] *= vars::vars[name];
+				i += 1;
+			}
+		}
+		cout << name << " = " << vars::vars[name] << endl;
+		return 0;
+	}
+
+	//开方
+	int srt(const float& num,const string& name)
+	{
+		int num2 = static_cast<int>(num);
+		if (num2 < 0)
+		{
+			cout << "error 06: Negative index" << endl;
+		}else if (vars::vars[name] < 0 && num2 % 2 == 0)
+		{
+			cout << "error 05: Unsupported digital domains" << endl;
+		}else if (vars::vars[name] > 0 && num2 % 2 != 0)
+		{
+			num2 = -num2;
+			vars::vars[name] = std::pow(vars::vars[name], 1.0/num2);
+		}else{
+			vars::vars[name] = std::pow(vars::vars[name], 1.0/num2);
 		}
 		return 0;
 	}
@@ -50,17 +126,47 @@ namespace func{
 
 int command_switch(const string& command,const string& a,const string& b)
 {
-	if(a.empty()){
-		cout << "error 02: first input is empty " << endl;
-		return 1;
-	}
 	if(command == "print"){
 		func::print(a);
 		return 0;
 	}
+	float num = 0;
+	if (not func::isnumber(a,num))
+	{
+		cout << "error 04: use non-real numbers to assign values" << endl;
+		return 1;
+	}
+	if(a.empty()){
+		cout << "error 02: first input is empty " << endl;
+		return 1;
+	}
 	if(command == "set"){
-		func::set(a, b);
+		func::set(num, b);
 		return 0;
+	}
+	if (command == "add")
+	{
+		func::add(num, b);
+	}
+	if (command == "sub")
+	{
+		func::sub(num, b);
+	}
+	if (command == "mul")
+	{
+		func::mul(num, b);
+	}
+	if (command == "div")
+	{
+		func::div(num, b);
+	}
+	if (command == "fang")
+	{
+		func::fang(num, b);
+	}
+	if (command == "srt")
+	{
+		func::srt(num, b);
 	}
 	cout << "error 01: incorrect command" <<endl;
 	return 1;
@@ -76,7 +182,7 @@ int input_command(){
 }
 
 [[noreturn]] int main(){
-	string version = "INDEV VERSION";
+	string version = "InDev VERSION";
 	cout << "Welcome to use the COMMAND LINE CALCULATOR (C++ remake version)" << version << "!" << endl;
 	while(true){
 		input_command();
